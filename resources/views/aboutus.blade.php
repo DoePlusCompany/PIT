c7d719127e957afcc296ef6536f7d402265b8cd3
DanielVeiga <danielveigarodriguess@gmail.com>
2024-11-27 12:06:17 -0300

    Ajustes na pagina Sobre Nos

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,24 +1,51 @@
 @extends('layouts.app')
 
-@section('conteudo')
+<header id="home-header-about-us">
 
-<div class="container">
+    <nav id="home-header-left">
+        <h2>DOE+</h2>
+        <ul id="home-header-items">
+            <li> <a href="/">Inicio</a></li>
 
-    <main id="main-content">
+            <li><a href="/donations">Doações</a></li>
 
-        <section id="section-history">
-            <h2 id="history-heading">Nossa História</h2>
-            <p id="history-paragraph-1">
-            Doe+ foi fundada recentemente com o objetivo de transformar o processo de doação, tornando-o mais acessível e eficiente para todos. Nossa jornada começou com um grupo de entusiastas da solidariedade, unidos pela missão de facilitar a conexão entre doadores e beneficiários.
+            <li><a href="/sac/create">Contato</a></li>
+        </ul>
+    </nav>
+
+    <div id="hero-main-right">
+        <div id="button-div-header-home-page">
+            <a href="/login" id="button-header-home-page">LOGIN</a>
+        </div>
+
+</header>
+
+<div class="about-us-container">
+
+    <main class="main-content">
+
+        <!-- Seção Nossa História -->
+        <section class="section-history">
+            <h2 class="section-heading">Nossa História</h2>
+            <p class="history-paragraph">
+                Doe+ foi fundada recentemente com o objetivo de transformar o processo de doação, tornando-o mais
+                acessível e eficiente para todos. Nossa jornada começou com um grupo de entusiastas da solidariedade,
+                unidos pela missão de facilitar a conexão entre doadores e beneficiários.
             </p>
-            <p id="history-paragraph-2">
-            Desde o nosso início, fizemos progressos significativos ao estabelecer uma plataforma intuitiva que simplifica as doações e maximiza o impacto social. Em pouco tempo, conseguimos alcançar marcos importantes, como parcerias estratégicas com organizações chave e a implementação de novas ferramentas que aprimoram a experiência de doação.
-            Hoje, continuamos a trabalhar com paixão e dedicação para expandir nossa rede de doadores e parceiros, e para apoiar ainda mais causas que fazem a diferença na vida das pessoas. Nossa missão é seguir promovendo a generosidade e criando um impacto positivo cada vez maior na sociedade.
+            <p class="history-paragraph">
+                Desde o nosso início, fizemos progressos significativos ao estabelecer uma plataforma intuitiva que
+                simplifica as doações e maximiza o impacto social. Em pouco tempo, conseguimos alcançar marcos
+                importantes, como parcerias estratégicas com organizações chave e a implementação de novas ferramentas
+                que aprimoram a experiência de doação.
+                Hoje, continuamos a trabalhar com paixão e dedicação para expandir nossa rede de doadores e parceiros, e
+                para apoiar ainda mais causas que fazem a diferença na vida das pessoas. Nossa missão é seguir
+                promovendo a generosidade e criando um impacto positivo cada vez maior na sociedade.
             </p>
         </section>
 
-        <section id="section-founders">
-            <h2 id="founders-heading">Nossos Fundadores</h2>
+        <!-- Seção Nossos Fundadores -->
+        <section class="section-founders">
+            <h2 class="section-heading">Nossos Fundadores</h2>
 
             <div class="founder-profile">
                 <h3 class="founder-name">Arthur Martins</h3>
@@ -27,25 +54,74 @@
 
             <div class="founder-profile">
                 <h3 class="founder-name">Caio de Souza</h3>
-                <p class="founder-bio" id="founder-bio-2">Designer & Front-end developer</p>
+                <p class="founder-bio">Designer & Front-end developer</p>
             </div>
 
             <div class="founder-profile">
                 <h3 class="founder-name">Daniel Veiga</h3>
-                <p class="founder-bio" id="founder-bio-2">Back-end developer</p>
+                <p class="founder-bio">Back-end developer</p>
             </div>
 
             <div class="founder-profile">
                 <h3 class="founder-name">Davi Lavarini</h3>
-                <p class="founder-bio" id="founder-bio-2">Front-end developer</p>
+                <p class="founder-bio">Front-end developer</p>
             </div>
 
             <div class="founder-profile">
                 <h3 class="founder-name">Kauan Gabriel</h3>
-                <p class="founder-bio" id="founder-bio-2">Designer & Video Maker</p>
+                <p class="founder-bio">Back-end developer</p>
             </div>
-
         </section>
     </main>
 </div>
-@endsection
+
+<section id="footer">
+    <div id="footer-wrapper">
+        <div id="footer-wrapper-left">
+            <div id="footer-wrapper-itens">
+                <ul>
+                    <li>Siga-nos</li>
+
+                    <li>WhatsApp</li>
+                    <li>Instagram</li>
+                    <li>Linkedin</li>
+                    <li>Facebook</li>
+                    <li>TikTok</li>
+                </ul>
+                <ul>
+                    <li>Idealizadores</li>
+
+                    <li>Arthur Martins</li>
+                    <li>Daniel Isaac</li>
+                    <li>Caio de Souza</li>
+                    <li>Davi Lavarini</li>
+                    <li>Kauan Gabriel</li>
+                </ul>
+                <ul>
+                    <li>Links rápidos</li>
+
+                    <li><a class="speedlinks" href="/aboutus">Sobre Nós</a></li>
+                    <li><a class="speedlinks" href="/sac/create">SAC</a></li>
+                    <li><a class="speedlinks" href="">Pages</a></li>
+                    <li><a class="speedlinks" href="">Pages</a></li>
+                    <li><a class="speedlinks" href="">Pages</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="footer-wrapper-right">
+            <div id="fale-conosco">
+                <h2>
+                    Fale conosco
+                </h2>
+                <div>
+                    <p>
+                        Email: teste@gmail.com
+                    </p>
+                    <p>
+                        Telefone: telefone@gmail.com
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
